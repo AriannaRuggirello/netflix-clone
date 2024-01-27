3f8b5da2a8eb773daf0b9619f149f48016db0388
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2024-01-27 14:24:22 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './style/App.css';
 import './style/Row.css'
 import './style/Banner.css'
+import './style/Navbar.css'
+
 
 import Row from "./components/Row";
 import requests from "./requests";
-import Banner from "./components/Banner"
+import Banner from "./components/Banner";
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="app">
       {/* navbar */}
+      <Navbar />
       <Banner />
       <Row
         title='NETFLIX ORIGINALS'
